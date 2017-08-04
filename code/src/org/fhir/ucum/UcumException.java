44a64b33b941e9ff4cb1ea56ab7ff136f266fd19
Grahame Grieve <grahameg@gmail.com>
2017-08-05 07:38:37 +1000

    Initial Commit

---
+++ code/src/org/fhir/ucum/UcumException.java
@@ -0,0 +1,27 @@
+package org.fhir.ucum;
+
+public class UcumException extends Exception {
+
+  public UcumException() {
+    super();
+  }
+
+  public UcumException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+    super(arg0, arg1, arg2, arg3);
+  }
+
+  public UcumException(String arg0, Throwable arg1) {
+    super(arg0, arg1);
+  }
+
+  public UcumException(String arg0) {
+    super(arg0);
+  }
+
+  public UcumException(Throwable arg0) {
+    super(arg0);
+  }
+
+ 
+
+}
