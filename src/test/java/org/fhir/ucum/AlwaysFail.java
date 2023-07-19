617e448425f9ed7feb3d86d1986d0ac95986772a
dotasek <dotasek.dev@gmail.com>
2023-07-19 16:43:54 -0400

    Test PR pipeline with breaking test

---
+++ src/test/java/org/fhir/ucum/AlwaysFail.java
@@ -0,0 +1,13 @@
+package org.fhir.ucum;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class AlwaysFail {
+    @Test
+    public void fail() {
+        assertTrue(false);
+    }
+}
