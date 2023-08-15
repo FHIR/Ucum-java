fba141cb7dbdc32708ac27429444fcfd7e886555
dotasek <david.otasek@smilecdr.com>
2023-08-15 09:35:27 -0400

    Failing test

---
+++ src/test/java/org/fhir/ucum/UcumCanonicalTest.java
@@ -0,0 +1,21 @@
+package org.fhir.ucum;
+
+
+
+
+
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.Assert.assertEquals;
+
+public class UcumCanonicalTest extends UcumServiceTest {
+
+
+    @Test
+    public void testGCanonical() throws FileNotFoundException, UcumException {
+        Pair actual = ucumService.getCanonicalForm(new Pair(new Decimal(1), "g"));
+        assertEquals("1 g", actual.toString());
+    }
+}
