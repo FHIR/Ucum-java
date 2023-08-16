03042f8b85ae8bbbda85d55dcf74fcd896884001
dotasek <david.otasek@smilecdr.com>
2023-08-16 10:27:58 -0400

    Refactor UcumFunctionalTests, make JUnit5 parameterized

--- src/test/java/org/fhir/ucum/UcumFunctionalTests.java
+++ src/test/java/org/fhir/ucum/UcumFunctionalTests.java
@@ -33,99 +33,105 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 package org.fhir.ucum;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-
-
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.fhir.ucum.utils.XmlUtils;
-import org.junit.Assert;
-
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
-public class UcumFunctionalTests {
-
-  @Test
-  public void testSuite() throws IOException, UcumException, ParserConfigurationException, SAXException {
-    UcumFunctionalTests worker = new UcumFunctionalTests();
-    worker.setDefinitions("src/main/resources/ucum-essence.xml");
-    worker.setTests("src/test/resources/UcumFunctionalTests.xml");
-    System.out.println("Defitions: " + worker.getDefinitions() + "\n" + "Tests: " + worker.getTests());
-    worker.execute();
-
-  }
+import static org.junit.jupiter.api.Assertions.fail;
 
 
-  private String definitions;
-  private String tests;
-  public String getDefinitions() {
-    return definitions;
-  }
-  public void setDefinitions(String definitions) {
-    this.definitions = definitions;
-  }
-  public String getTests() {
-    return tests;
-  }
-  public void setTests(String tests) {
-    this.tests = tests;
-  }
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UcumFunctionalTests extends UcumServiceTest {
 
-
-  private UcumService ucumSvc;
-  private int errCount;
-
-  private void execute() throws IOException, UcumException, ParserConfigurationException, SAXException  {
-    ucumSvc = new UcumEssenceService(definitions);
-    errCount = 0;
-    Document doc = XmlUtils.parseDOM(new FileInputStream(tests));
-    Element element = doc.getDocumentElement();
-
-    if (!element.getNodeName().equals("ucumTests"))
-      throw new UcumException("Unable to process XML document: expected 'ucumTests' but found '"+element.getNodeName()+"'");
-
-    Element focus = XmlUtils.getFirstChild(element);
-    while (focus != null) {
-      if (focus.getNodeName().equals("history"))
-        ;
-      else if (focus.getNodeName().equals("validation"))
-        runValidationTests(focus);
-      else if (focus.getNodeName().equals("displayNameGeneration"))
-        runDisplayNameGeneration(focus);
-      else if (focus.getNodeName().equals("conversion"))
-        runConversion(focus);
-      else if (focus.getNodeName().equals("multiplication"))
-        runMultiplication(focus);
-      else if (focus.getNodeName().equals("division"))
-        runDivision(focus);
-      else
-        throw new UcumException("unknown element name "+focus.getNodeName());
-      focus = XmlUtils.getNextSibling(focus);
+    private static class UcumFunctionalTestException extends Exception {
+        public UcumFunctionalTestException(String string) {
+            super(string);
+        }
     }
-    if (errCount > 0) {
-      System.err.println(Integer.toString(errCount)+" errors");
+    private enum FunctionalTestType {
+        HISTORY,
+        VALIDATION,
+        DISPLAY_NAME_GENERATION,
+        CONVERSION,
+        DIVISION,
+        MULTIPLICATION
     }
-    Assert.assertEquals(0, errCount);
-  }
 
-  private void runMultiplication(Element x) throws IOException, UcumException  {
-    for (Element e : XmlUtils.getNamedChildren(x, "case")) {
-      runMultiplicationCase(e);      
+    private static FunctionalTestType getFunctionalTestType(String typeString) throws UcumFunctionalTestException {
+        if (typeString.equals("history"))
+            return FunctionalTestType.HISTORY;
+        else if (typeString.equals("validation"))
+            return FunctionalTestType.VALIDATION;
+        else if (typeString.equals("displayNameGeneration"))
+            return FunctionalTestType.DISPLAY_NAME_GENERATION;
+        else if (typeString.equals("conversion"))
+            return  FunctionalTestType.CONVERSION;
+        else if (typeString.equals("multiplication"))
+            return FunctionalTestType.MULTIPLICATION;
+        else if (typeString.equals("division"))
+            return FunctionalTestType.DIVISION;
+        else
+            throw new UcumFunctionalTestException("unknown element name "+ typeString);
     }
-  }
+  public static Stream<Arguments> getTestXMLElements() throws UcumFunctionalTestException, IOException, ParserConfigurationException, SAXException {
+      List<Arguments> elements = new ArrayList<>();
+      String tests = "src/test/resources/UcumFunctionalTests.xml";
+      Document doc = XmlUtils.parseDOM(new FileInputStream(tests));
+      Element element = doc.getDocumentElement();
 
-  private void runDivision(Element x) throws IOException, UcumException  {
-    for (Element e : XmlUtils.getNamedChildren(x, "case")) {
-      runDivisionCase(e);      
+      if (!element.getNodeName().equals("ucumTests"))
+          throw new UcumFunctionalTestException("Unable to process XML document: expected 'ucumTests' but found '"+element.getNodeName()+"'");
+
+      Element focus = XmlUtils.getFirstChild(element);
+      while (focus != null) {
+
+          String testTypeString = focus.getNodeName();
+          FunctionalTestType testType = getFunctionalTestType(testTypeString);
+          for (Element testCase : XmlUtils.getNamedChildren(focus, "case"))
+          {
+              String testId = testCase.getAttribute("id");
+            elements.add(Arguments.of(testTypeString + ": " + testId, testType, testCase));
+          }
+          focus = XmlUtils.getNextSibling(focus);
+
+      }
+      return elements.stream();
+  }
+    @DisplayName("UCUM Functional Tests loaded from UcumFunctionalTests.xml")
+    @ParameterizedTest(name = "{index}: {0}")
+    @MethodSource("getTestXMLElements")
+    public void testXmlElement(String testName, FunctionalTestType testType, Element testCase) throws IOException, UcumException, UcumFunctionalTestException {
+        if (testType.equals(FunctionalTestType.HISTORY))
+            // Test history element. Not a test. Do nothing.
+            ;
+        else if (testType.equals(FunctionalTestType.VALIDATION))
+          runValidationCase(testCase);
+        else if (testType.equals(FunctionalTestType.DISPLAY_NAME_GENERATION))
+          runDisplayNameGenerationCase(testCase);
+        else if (testType.equals(FunctionalTestType.CONVERSION))
+          runConversionCase(testCase);
+        else if (testType.equals(FunctionalTestType.MULTIPLICATION))
+          runMultiplicationCase(testCase);
+        else if (testType.equals(FunctionalTestType.DIVISION))
+          runDivisionCase(testCase);
+        else
+          throw new UcumFunctionalTestException("unknown test type "+ testCase.getNodeName());
     }
-  }
 
-  private void runMultiplicationCase(Element x) throws IOException, UcumException  {
+  private void runMultiplicationCase(Element x) throws UcumException  {
 
     String id = x.getAttribute("id");
     String v1 = x.getAttribute("v1");
@@ -137,19 +143,18 @@ public class UcumFunctionalTests {
 
     Pair o1 = new Pair(new Decimal(v1), u1);
     Pair o2 = new Pair(new Decimal(v2), u2);
-    Pair o3 = ucumSvc.multiply(o1, o2);
+    Pair o3 = ucumService.multiply(o1, o2);
 
     debug("Multiplication Test "+id+": the value '"+v1+" "+u1+"' * '"+v2+" "+u2+"' ==> "+o3.getValue().toString()+" "+o3.getCode());
 
     // if (!res.toPlainString().equals(outcome)) { - that assumes that we can get the precision right, which we can't
     if (o3.getValue().comparesTo(new Decimal(vRes)) != 0 || !o3.getCode().equals(uRes)) {
-      errCount++;
-      System.err.println("Test "+id+": The value '"+vRes+" "+uRes+"' was expected, but the result was "+o3.getValue().toString()+" "+o3.getCode());
+      fail("Test "+id+": The value '"+vRes+" "+uRes+"' was expected, but the result was "+o3.getValue().toString()+" "+o3.getCode());
     }
 
   }
 
-  private void runDivisionCase(Element x) throws IOException, UcumException  {
+  private void runDivisionCase(Element x) throws UcumException  {
 
     String id = x.getAttribute("id");
     String v1 = x.getAttribute("v1");
@@ -161,26 +166,19 @@ public class UcumFunctionalTests {
 
     Pair o1 = new Pair(new Decimal(v1), u1);
     Pair o2 = new Pair(new Decimal(v2), u2);
-    Pair o3 = ucumSvc.divideBy(o1, o2);
+    Pair o3 = ucumService.divideBy(o1, o2);
 
     debug("Division Test "+id+": the value '"+v1+" "+u1+"' * '"+v2+" "+u2+"' ==> "+o3.getValue().toString()+" "+o3.getCode());
 
     // if (!res.toPlainString().equals(outcome)) { - that assumes that we can get the precision right, which we can't
     if (o3.getValue().comparesTo(new Decimal(vRes)) != 0 || !o3.getCode().equals(uRes)) {
-      errCount++;
-      System.err.println("Test "+id+": The value '"+vRes+" "+uRes+"' was expected, but the result was "+o3.getValue().toString()+" "+o3.getCode());
+      fail("Test "+id+": The value '"+vRes+" "+uRes+"' was expected, but the result was "+o3.getValue().toString()+" "+o3.getCode());
     }
 
   }
 
 
-  private void runConversion(Element x) throws IOException, UcumException  {
-    for (Element e : XmlUtils.getNamedChildren(x, "case")) {
-      runConversionCase(e);      
-    }
-  }
-
-  private void runConversionCase(Element x) throws IOException, UcumException  {
+  private void runConversionCase(Element x) throws UcumException  {
 
     String id = x.getAttribute("id");
     String value = x.getAttribute("value");
@@ -188,13 +186,12 @@ public class UcumFunctionalTests {
     String dstUnit = x.getAttribute("dstUnit");
     String outcome = x.getAttribute("outcome");
     System.out.println("case "+id+": "+value+" "+srcUnit+" -> "+outcome+" "+dstUnit);
-    Decimal res = ucumSvc.convert(new Decimal(value), srcUnit, dstUnit);
+    Decimal res = ucumService.convert(new Decimal(value), srcUnit, dstUnit);
     debug("Convert Test "+id+": the value '"+value+" "+srcUnit+"' ==> "+res.toString()+" "+dstUnit);
 
     // if (!res.toPlainString().equals(outcome)) { - that assumes that we can get the precision right, which we can't
     if (res.comparesTo(new Decimal(outcome)) != 0) {
-      errCount++;
-      System.err.println("Test "+id+": The value '"+outcome+"' was expected the result was "+res.toString());
+      fail("Test "+id+": The value '"+outcome+"' was expected the result was "+res);
     }
   }
 
@@ -203,39 +200,27 @@ public class UcumFunctionalTests {
     // System.out.println(string);
 
   }
-  private void runDisplayNameGeneration(Element x) throws IOException, UcumException  {
-    for (Element e : XmlUtils.getNamedChildren(x, "case")) {
-      runDisplayNameGenerationCase(e);      
-    }
-  }
 
-  private void runDisplayNameGenerationCase(Element x) throws IOException, UcumException  {
+  private void runDisplayNameGenerationCase(Element x) throws UcumException  {
     String id = x.getAttribute("id");
     String unit = x.getAttribute("unit");
     String display = x.getAttribute("display");
 
-    String res = ucumSvc.analyse(unit);
+    String res = ucumService.analyse(unit);
     debug("Analyse Test "+id+": the unit '"+unit+"' ==> "+res);
 
     if (!res.equals(display)) {
-      errCount++;
-      System.err.println("Test "+id+": The unit '"+unit+"' was expected to be displayed as '"+display+"', but was displayed as "+res);
+      fail("Test "+id+": The unit '"+unit+"' was expected to be displayed as '"+display+"', but was displayed as "+res);
     }
   }
 
-  private void runValidationTests(Element x) throws IOException  {
-    for (Element e : XmlUtils.getNamedChildren(x, "case")) {
-      runValidationCase(e);      
-    }
-  }
-
-  private void runValidationCase(Element x) throws IOException  {
+  private void runValidationCase(Element x) {
     String id = x.getAttribute("id");
     String unit = x.getAttribute("unit");
     boolean valid = "true".equals(x.getAttribute("valid"));
     String reason = x.getAttribute("reason");
 
-    String res = ucumSvc.validate(unit);
+    String res = ucumService.validate(unit);
     boolean result = res == null;
     if (result)
       debug("Validation Test "+id+": the unit '"+unit+"' is valid");
@@ -243,16 +228,13 @@ public class UcumFunctionalTests {
       debug("Validation Test "+id+": the unit '"+unit+"' is not valid because "+res);
 
     if (valid != result) {
-      errCount++;
       if (valid) {
-        Assert.assertTrue("Unit "+unit+" was expected to be valid, but was invalid ("+id+")", false);
-        System.err.println("Test "+id+": The unit '"+unit+"' was expected to be valid, but wasn't accepted");
+       fail("Unit "+unit+" was expected to be valid, but was invalid ("+id+")");
+
       } else {
-        Assert.assertTrue("Unit "+unit+" was expected to be invalid, but was valid ("+id+")", false);
-        System.err.println("Test "+id+": The unit '"+unit+"' was expected to be invalid because '"+reason+"', but was accepted");
+        fail("Unit "+unit+" was expected to be invalid, but was valid ("+id+")");
       }
     }
   }
-
 
 }
