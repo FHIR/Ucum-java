d2fe9891bb87b442d237f85dec6b54052bf67eae
dotasek <david.otasek@smilecdr.com>
2023-08-16 14:15:22 -0400

    JUnit4 is dead. Long live JUnit5.

--- src/test/java/org/fhir/ucum/UcumCanonicalTest.java
+++ src/test/java/org/fhir/ucum/UcumCanonicalTest.java
@@ -8,7 +8,8 @@ import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class UcumCanonicalTest extends UcumServiceTest {
 
